9f54196b8ab579f83add1a3adf26c06bd8a611bd
Hannah Gibson <hgibson1@bu.edu>
2014-11-08 23:14:00 -0500

    First Commit

---
+++ BlogReader/BlogPost.h
@@ -0,0 +1,13 @@
+//
+//  BlogPost.h
+//  BlogReader
+//
+//  Created by Hannah Gibson on 10/25/14.
+//  Copyright (c) 2014 Hannah Gibson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface BlogPost : NSObject
+
+@end
---
+++ BlogReader/BlogPost.m
@@ -0,0 +1,13 @@
+//
+//  BlogPost.m
+//  BlogReader
+//
+//  Created by Hannah Gibson on 10/25/14.
+//  Copyright (c) 2014 Hannah Gibson. All rights reserved.
+//
+
+#import "BlogPost.h"
+
+@implementation BlogPost
+
+@end
